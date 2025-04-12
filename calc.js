46e5d6f4c5e4cefae86c6a41905a6e0c92480c04
Pedro Paoleschi <pedrorpaoleschi@gmail.com>
2025-04-12 18:07:11 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,14 @@
+// alert("calc");
+// declaração de variáveis
+let num1 = 20;
+let num2 = 5;
+
+// Operação matemáteticas
+let soma = num1 + num2;
+let mult = num1 * num2;
+let sub = num1 - num2;
+
+// exibindo os resultados no console
+console.log("O resultado da soma é:" + soma);
+console.log("O resultado é:" * mult);
+console.log("O resultado da Sub é:" - sub);
